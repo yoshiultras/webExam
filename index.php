682c49cc89e3fad7c63325c8b68292ad29ffa8bb
yoshiultras <nikita.salivonov@yandex.ru>
2023-12-27 16:46:10 +0300

    Сверстана главная страница

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Доставка</title>
+	<link rel="icon" type="image/x-icon" href="https://static-00.iconduck.com/assets.00/face-cool-icon-256x256-zhs2iw6t.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color:saddlebrown">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Доставка</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#description">Описание</a>
+                    </li>
+					<li class="nav-item">
+                        <a class="nav-link" href="#pros">Описание</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contacts">Контакты</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+	<div id="carouselExampleIndicators" width="200" height="300" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" width="200" height="300">
+                    <img src="https://sun9-74.userapi.com/impg/4LaikasnLCfsDdD5ZUv6JBGk1DwiPZ1MpInFKA/a6D6DJE20Gg.jpg?size=1280x322&quality=95&sign=7e2730822f72c5b12192441c684937a0&c_uniq_tag=NphiJRrwI1rmJlq5Y2nra2KReD_JQPBDujcpTuUywKc&type=album" class="d-block w-100" width="200" height="300">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://sun9-21.userapi.com/c855620/v855620934/c0275/UixWn6eQKpM.jpg" class="d-block w-100" width="200" height="300">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://sun9-15.userapi.com/impf/oIjmxkWjRo66k7TD2lq-95H69X1yCmL8L9oFpg/xB9BGguNKFw.jpg?size=1590x530&quality=95&crop=0,89,1000,333&sign=681a108562d6cb942a3e8417da173245&type=cover_group" class="d-block w-100" width="200" height="300">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Предыдущий</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Следующий</span>
+            </button>
+        </div>
+
+	<div class="container mt-5" id="pros">
+		<h2>Почему мы лучшие в своем деле</h2>
+		<div class="row">
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card">
+					<img src="https://media.geeksforgeeks.org/wp-content/uploads/20230911173805/What-is-Artiificial-Intelligence(AI).webp" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">ИИ</h5>
+						<p class="card-text">Мы используем искусственный интеллект, что обеспечивает лучший результат в данной сфере.</p>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card">
+					<img src="https://blog.calltracking.ru/wp-content/uploads/2022/05/3-1.png" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Рентабельность</h5>
+						<p class="card-text">Наша компания постоянно поддерживает и дорабатывает продукт.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card">
+					<img src="https://online.spbu.ru/wp-content/uploads/2020/10/cover_effect_commun_ASPIR-935x590.jpg" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Комуникативность</h5>
+						<p class="card-text">Мы всегда готовы выслушать ваши потребности и настроены на долгосрочное сотрудничество, чтобы помочь вам достичь ваших целей в сфере IT.</p>
+
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="row">
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card">
+					<img src="https://media.geeksforgeeks.org/wp-content/uploads/20230911173805/What-is-Artiificial-Intelligence(AI).webp" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">ИИ</h5>
+						<p class="card-text">Мы используем искусственный интеллект, что обеспечивает лучший результат в данной сфере.</p>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card">
+					<img src="https://blog.calltracking.ru/wp-content/uploads/2022/05/3-1.png" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Рентабельность</h5>
+						<p class="card-text">Наша компания постоянно поддерживает и дорабатывает продукт.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card">
+					<img src="https://online.spbu.ru/wp-content/uploads/2020/10/cover_effect_commun_ASPIR-935x590.jpg" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Комуникативность</h5>
+						<p class="card-text">Мы всегда готовы выслушать ваши потребности и настроены на долгосрочное сотрудничество, чтобы помочь вам достичь ваших целей в сфере IT.</p>
+
+					</div>
+				</div>
+			</div>
+		
+		</div>
+	</div>
+
+    <div style="background-color:yellow">
+		<div class="container mt-5">
+			<h1>Выбор предприятия общественного питания</h1>
+			<div class="input-group mb-3">
+			<input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+			</div>
+
+			<div class="input-group mb-3">
+			<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+			</div>
+
+			<div class="input-group mb-3">
+			<input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+			</div>
+
+			<div class="input-group">
+			<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+			</div>
+			<table class="table">
+				<thead>
+					<tr>
+					<th scope="col">Название</th>
+					<th scope="col">Тип</th>
+					<th scope="col">Адрес</th>
+					<th scope="col">Действие</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+					</tr>
+					<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+					</tr>
+					<tr>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<div class="container mt-5">
+		<h1>Меню</h1>
+		<div class="row text-white">
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card bg-primary"  style="border-color:skyblue">
+					<img src="https://media.geeksforgeeks.org/wp-content/uploads/20230911173805/What-is-Artiificial-Intelligence(AI).webp" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">ИИ</h5>
+						<p class="card-text">Мы используем искусственный интеллект, что обеспечивает лучший результат в данной сфере.</p>
+						<h6>Цена</h6>
+						<div class="row " id="menuCntl">
+							<div class="col-md-4">
+								<button class="btn" style="background-color:lime">-</button>
+							</div>
+							<div class="col-md-4">
+								<span class="count">1</span>
+							</div>
+							<div class="col-md-4">
+								<button class="btn" style="background-color:lime">+</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card bg-primary">
+					<img src="https://blog.calltracking.ru/wp-content/uploads/2022/05/3-1.png" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Рентабельность</h5>
+						<p class="card-text">Наша компания постоянно поддерживает и дорабатывает продукт.</p>
+						<h6>Цена</h6>
+						<div class="row " id="menuCntl">
+							<div class="col-md-4">
+								<button class="btn" style="background-color:lime">-</button>
+							</div>
+							<div class="col-md-4">
+								<span class="count">1</span>
+							</div>
+							<div class="col-md-4">
+								<button class="btn" style="background-color:lime">+</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex align-items-stretch">
+				<div class="card bg-primary">
+					<img src="https://online.spbu.ru/wp-content/uploads/2020/10/cover_effect_commun_ASPIR-935x590.jpg" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Сет 1</h5>
+						<p class="card-text">1, 2, 3</p>
+						<h6>Цена</h6>
+						<div class="row " id="menuCntl">
+							<div class="col-md-4">
+								<button class="btn" style="background-color:lime">-</button>
+							</div>
+							<div class="col-md-4">
+								<span class="count">1</span>
+							</div>
+							<div class="col-md-4">
+								<button class="btn" style="background-color:lime">+</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+
+	<div class="text-white" style="background-color:yellowgreen">
+		<div class="container mt-5">
+			<h1>Дополнительные функции</h1>
+			<div class="form-check form-check">
+				<input
+					class="form-check-input"
+					type="checkbox"
+					id=""
+					value="option1"
+				/>
+				<label class="form-check-label" for="">first</label>
+			</div>
+			<div class="form-check form-check">
+				<input
+					class="form-check-input"
+					type="checkbox"
+					id=""
+					value="option2"
+				/>
+				<label class="form-check-label" for="">second</label>
+			</div>
+		</div>
+		<div class="container mt-5">
+			<span>Итого:</span>
+			<button class="btn" style="background-color:violet">Заказ</button>
+		</div>
+		<br>
+	</div>
+
+    <footer class="footer" style="background-color:saddlebrown">
+		<br>
+        <div class="container text-white">
+            <p>Kakaya-to company© 2023</p>
+        </div>
+		<br>
+    </footer>
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
